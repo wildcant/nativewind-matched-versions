4b158a656c53dd2d5160925c86a613d8d2200b47
Wilmer Cantillo <dev@wilmercantillo.com>
2024-01-23 20:35:54 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.js'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
